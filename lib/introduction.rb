86621f9fb2149ab95e8d50a8f8a887b1d93a8121
Logan McGuire <lrmcguire93@gmail.com>
2020-04-14 21:11:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,14 @@
+def introduction (name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language (name, code_lang)
+  puts "Hi, my name is #{name} and I am learning to program in #{code_lang}."
+end
+
+def introduction_with_language_optional (name, )
+  
+end
 
 
 
